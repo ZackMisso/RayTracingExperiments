dec02deb4509938f89805e56d723e811b4f68ebf
ZackMisso <zack441@mac.com>
2015-03-21 20:30:33 -0500

    Initial Commit

---
+++ RayTracing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracing
+//
+//  Created by Zackary T Misso on 3/21/15.
+//  Copyright (c) 2015 Zackary Misso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
